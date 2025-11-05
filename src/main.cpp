0d037660697675bab2373e6b6c3242eee1b0ae0d
nomo <51972322+nomoemptypointer@users.noreply.github.com>
2025-11-05 18:28:06 +0100

    Hello world

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <ostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "Hello World!" << std::endl;
+    std::cout << BUILD_VERSION << std::endl;
+    return 0;
+}
